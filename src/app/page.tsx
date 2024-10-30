6fd2934e72896fe7f6c70db78bc8ea8ac70c355c
eros borba <borba.eros@gmail.com>
2024-10-30 12:31:43 -0300

    Adicionado meio de pagamento e db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 // src/app/page.tsx
-import Hero from '@/components/sections/Hero'
-import HowItWorks from '@/components/sections/HowItWorks'
-import Pricing from '@/components/sections/Pricing'
-import CallToAction from '@/components/sections/CallToAction'
-import Footer from '@/components/sections/Footer'
+import Hero from './components/sections/Hero'
+import HowItWorks from './components/sections/HowItWorks'
+import Pricing from './components/sections/Pricing'
+import CallToAction from './components/sections/CallToAction'
+import Footer from './components/sections/Footer'
 
 export default function Home() {
   return (
